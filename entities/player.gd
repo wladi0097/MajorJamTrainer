405116e3901696459439c83765d6940dfa8b3056
wladi <wladi0097@googlemail.com>
2021-08-11 22:19:05 +0200

    init

---
+++ entities/player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody
+
+onready var camera = $Pivot/Camera
+var gravity = -5
+var max_speed = 2
+var mouse_sensitivity = 0.004
+var jump_speed = 2
+
+var jump = false
+var velocity = Vector3()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event.is_action_pressed("click"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func get_input():
+	jump = false
+	if Input.is_action_just_pressed("move_jump"):
+		jump = true
+	
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forwards"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("move_backwards"):
+		input_dir += global_transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		input_dir += global_transform.basis.x
+	input_dir = input_dir.normalized()
+	return input_dir
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	
+	if jump and is_on_floor():
+		velocity.y = jump_speed
