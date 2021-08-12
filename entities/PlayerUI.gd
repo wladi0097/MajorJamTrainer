366a6bd0f6d9421e162566e3ceb0064ad1e386fc
Justin Schoenberg <jschoenberg.itsb-b2020@fh-salzburg.ac.at>
2021-08-12 22:48:23 +0200

    Allow UI clicks

--- entities/PlayerUI.gd
+++ entities/PlayerUI.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 
 onready var rightBlackBar = $RightBar
 onready var hackUi : CanvasLayer = $LeftBar/CanvasLayer
+onready var hackUiImage : TextureRect = $LeftBar/CanvasLayer/TextureRect
 onready var hackUiButton : Button = $LeftBar/MaximizeButton
 
 var isHackUiOpen = false
@@ -24,29 +25,26 @@ func _process(delta):
 		rightBlackBar.set_v_size_flags( SIZE_FILL | SIZE_EXPAND )
 		hackUi.offset = Vector2(-500, 0)
 		hasHackUiOpened = false
-	pass
 
 func _input(event):	
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		return
+
 	if event.is_action_pressed("click"):
 		var hackUiButtonRect = hackUiButton.get_rect()
-		if hackUiButtonRect.has_point(event.position):
-			pass
+		var hackUiImageRect = hackUiImage.get_rect()
+		if hackUiButtonRect.has_point(event.position) || hackUiImageRect.has_point(event.position):
+			return
 			
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass
-
 
 func _on_MaximizeButton_button_down():
 	if isHackUiOpen:
 		isHackUiOpen = false
 	else:
 		isHackUiOpen = true
-	pass
-
 
 func _on_MinizeButton_button_down():
 	isHackUiOpen = false
-	pass
