290006e6692b221774b39853dd4d22d6a55f481e
wladi <wladi0097@googlemail.com>
2021-08-12 14:17:47 +0200

    add death laser

--- entities/Block.gd
+++ entities/Block.gd
@@ -1,5 +1,20 @@
 extends RigidBody
 
+var initialPosition = Transform()
+onready var dieParticles = $DieParticles
+onready var mainMesh = $MeshInstance
 
 func _ready():
-	pass # Replace with function body.
+	self.initialPosition = self.global_transform
+	
+func resetPosition():
+	self.mainMesh.visible = false
+	self.dieParticles.emitting = true
+	yield(get_tree().create_timer(0.2), "timeout")
+	
+	self.transform = self.initialPosition
+	self.force_update_transform()
+	self.mainMesh.visible = true
+	
+func _on_DeathTrigger_body_entered(body):
+	self.resetPosition()
