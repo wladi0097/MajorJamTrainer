461c1504ff4f28a11f04251640873e5ca6cfa8d5
wladi <wladi0097@googlemail.com>
2021-08-13 20:11:15 +0200

    add credits

--- entities/PlayerUI.gd
+++ entities/PlayerUI.gd
@@ -2,29 +2,12 @@ extends HBoxContainer
 
 
 onready var rightBlackBar = $RightBar
-onready var hackUi : CanvasLayer = $LeftBar/CanvasLayer
-onready var hackUiImage : TextureRect = $LeftBar/CanvasLayer/TextureRect
+onready var hackUi : CanvasLayer = $LeftBar/HackUi
+onready var credits : CanvasLayer = $LeftBar/Credits
+onready var hackUiImage : TextureRect = $LeftBar/HackUi/TextureRect
 onready var hackUiButton : Button = $LeftBar/MaximizeButton
 
 var isHackUiOpen = false
-var hasHackUiOpened = false
-
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if isHackUiOpen && !hasHackUiOpened:
-		rightBlackBar.set_h_size_flags( SIZE_FILL )
-		rightBlackBar.set_v_size_flags( SIZE_FILL )
-		hackUi.offset = Vector2(0, 0)
-		hasHackUiOpened = true
-	
-	if !isHackUiOpen && hasHackUiOpened:
-		rightBlackBar.set_h_size_flags( SIZE_FILL | SIZE_EXPAND )
-		rightBlackBar.set_v_size_flags( SIZE_FILL | SIZE_EXPAND )
-		hackUi.offset = Vector2(-500, 0)
-		hasHackUiOpened = false
 
 func _input(event):	
 	if event.is_action_pressed("ui_cancel"):
@@ -32,6 +15,8 @@ func _input(event):
 		return
 
 	if event.is_action_pressed("click"):
+		credits.offset = Vector2(-500, 0)
+		
 		var hackUiButtonRect = hackUiButton.get_rect()
 		var hackUiImageRect = hackUiImage.get_rect()
 		if hackUiButtonRect.has_point(event.position) || hackUiImageRect.has_point(event.position):
@@ -40,11 +25,27 @@ func _input(event):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func openHackMenu():
+	rightBlackBar.set_h_size_flags( SIZE_FILL )
+	rightBlackBar.set_v_size_flags( SIZE_FILL )
+	hackUi.offset = Vector2(0, 0)
+	isHackUiOpen = true
+	
+func closeHackMenu():
+	rightBlackBar.set_h_size_flags( SIZE_FILL | SIZE_EXPAND )
+	rightBlackBar.set_v_size_flags( SIZE_FILL | SIZE_EXPAND )
+	hackUi.offset = Vector2(-500, 0)
+	isHackUiOpen = false
+
 func _on_MaximizeButton_button_down():
 	if isHackUiOpen:
-		isHackUiOpen = false
+		closeHackMenu()
 	else:
-		isHackUiOpen = true
+		openHackMenu()
 
 func _on_MinizeButton_button_down():
-	isHackUiOpen = false
+	closeHackMenu()
+
+
+func _on_CreditsButton_button_up():
+	credits.offset = Vector2(0, 0)
